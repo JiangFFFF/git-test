7380c99f74fff78ba990f203e6a56ea0769a96cd
Jiang <1820920433@qq.com>
2021-10-10 14:30:26 +0800

    first commit

---
+++ src/main/java/com/jiang/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.jiang.git;
+
+/**
+ * @author jiang
+ * @create 2021-10-10-2:07 下午
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
